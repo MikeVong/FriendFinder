f6940d2112dd96ed0f3880e1ff6365899d4f783e
Mike Vong <mv910@hotmail.com>
2020-04-28 18:50:33 -0700

    fix pathing

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var friendsData = 
+    {
+      name: "Mike",
+      photo: "https://pisum.photos/200",
+      score: [1,2,3,4,5,1,2,3,4,5]
+    };
+
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsData;
