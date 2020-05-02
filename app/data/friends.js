507ecbc220685e31213e716963848561e64e27d4
Mike Vong <mv910@hotmail.com>
2020-05-02 13:16:53 -0700

    upload before heroku

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,12 +5,28 @@
 // But you could have it be an empty array as well.
 // ===============================================================================
 
-var friendsData = 
+var friendsData = [
     {
       name: "Mike",
-      photo: "https://pisum.photos/200",
+      photo: "https://picsum.photos/200",
       score: [1,2,3,4,5,1,2,3,4,5]
-    };
+    },
+    {
+      name: "John",
+      photo: "https://picsum.photos/200",
+      score: [5,2,3,4,5,2,3,3,4,5]
+    },
+    {
+      name: "Matt",
+      photo: "https://picsum.photos/200",
+      score: [4,2,5,2,2,1,3,3,2,5]
+    },
+    {
+      name: "Howard",
+      photo: "https://picsum.photos/200",
+      score: [1,4,4,4,5,5,5,5,1,5]
+    }
+    ];
 
   
   // Note how we export the array. This makes it accessible to other files using require.
